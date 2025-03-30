d9abc4f806de604606ba9b653717ed9e23c39ebe
umesh218 <umesh16517@gmail.com>
2025-03-30 18:57:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 provider "azurerm" {
   features {}
-subscription_id = "17abc602-7315-4600-a8f8-412c409d1d92"
+ subscription_id = "17abc602-7315-4600-a8f8-412c409d1d92"
+  tenant_id       = "10f1e709-440c-4a18-9025-699d00a15b85"
+
 
 }
 
